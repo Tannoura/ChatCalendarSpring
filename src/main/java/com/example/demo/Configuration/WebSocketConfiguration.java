61900d0857d7c2ea94ba7101ddaa32fdb6c47bfe
Tannoura <koussay.zarrouk@esprit.tn>
2025-02-03 03:56:30 +0100

    Notif

--- src/main/java/com/example/demo/Configuration/WebSocketConfiguration.java
+++ src/main/java/com/example/demo/Configuration/WebSocketConfiguration.java
@@ -2,7 +2,6 @@ package com.example.demo.Configuration;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -22,5 +21,7 @@ public void configureMessageBroker(MessageBrokerRegistry registry){
     registry.addEndpoint("/chat-socket")
             .setAllowedOrigins("http://localhost:4200")
             .withSockJS();
+
+        ;
     }
 }
--- src/main/java/com/example/demo/Configuration/WebSocketController.java
+++ src/main/java/com/example/demo/Configuration/WebSocketController.java
@@ -1,6 +1,7 @@
 package com.example.demo.Configuration;
 
 import com.example.demo.entities.ChatMessage;
+import com.example.demo.entities.Notification;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
@@ -16,4 +17,12 @@ public class WebSocketController {
         public ChatMessage chat(@DestinationVariable String roomId,ChatMessage message){
             return  new ChatMessage(message.getMessage(), message.getUser());
         }
+
+    // Ajoutez un nouveau mapping pour les notifications
+    @MessageMapping("/notify/{userId}")
+    @SendTo("/topic/notifications/{userId}")
+    public Notification notify(@DestinationVariable String userId, Notification notification) {
+        return notification;
+    }
+
 }
